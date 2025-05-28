7359b4ecfbef9252efbaed9a9444097f3db65d71
Lukas <116753496+FrostySL@users.noreply.github.com>
2025-05-28 18:51:46 +0200

    Remove home.php and update index.php to handle comments and display visit count

--- index.php
+++ index.php
@@ -1,32 +1,47 @@
-<?php
-    session_start();
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=	, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This is the login page <br>
-    <a href="home.php">This goes to the homepage</a><br>
 
-    <form action="index.php" method="post">
-        <label for="username">Username</label><br>
-        <input type="text" name="username"><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" name="login" value="login">
+<html lang="de">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Gästebuch</title>
+  </head>
+  <body>
+    <h1>Hinterlassen Sie einen Kommentar!</h1>
+    <p>Diese Seite wurde bereits 
+        <?php 
+          $counterDatei = 'counter.txt';
+          if (!file_exists($counterDatei)) {
+              file_put_contents($counterDatei, "0");
+          }
+          $counter = (int)file_get_contents($counterDatei);
+          $counter++;
+          file_put_contents($counterDatei, $counter);
+          echo $counter;
+        ?> 
+        mal aufgerufen. </p>
 
-
+    <p>Hier könnnen Sie uns Ihren Kommentar übermitteln:</p>
+    <form action = "./index.php" method="POST">
+        <input type="text" name="kommentar"> <br>
+        <input type="submit" value="Absenden">
     </form>
-</body>
+    <?php 
+      if (!empty($_POST['kommentar'])) {
+      $kommentar = htmlspecialchars($_POST['kommentar']);
+      echo "<p>Ihr Kommentar: $kommentar</p>";
+      file_put_contents("comments.txt", $kommentar . PHP_EOL, FILE_APPEND);
+      }
+    ?>
+    <h2>Bisherige Kommentare:</h2>
+    <ul>
+      <?php
+        if (file_exists("comments.txt")) {
+          $kommentare = file("comments.txt", FILE_IGNORE_NEW_LINES);
+          foreach ($kommentare as $k) {
+            echo "<li>" . htmlspecialchars($k) . "</li>";
+          }
+        }
+      ?>
+    </ul>
+  </body>
 </html>
-<?php
-    $_SESSION["username"] = $_POST["username"];
-    $_SESSION["password"] = $_POST["password"];
-
-    echo $_SESSION["username"] . "<br>";
-    echo $_SESSION["password"] . "<br>";
-?>
